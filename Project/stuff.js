d233eac31e2aaeb3140cb0def5b355919a46029a
Gray <xxbloodyhell478@gmail.com>
2016-07-11 08:55:04 -0400

    Added shit tier documentation
    Look man, Just. Take it as it is ok :(
    I should really mark my shit as NSFW with all it's cursing.
    I know how much cursing like, hurts peoples feelings or whatever. Anywho
    I don't have time for other peoples feelings as I barely have time to
    fix this god damn bot.
    Hope this helps Lido. -_-. Though I'm sure you'll let me know if it
    doesn't.

--- Project/stuff.js
+++ Project/stuff.js
@@ -1,20 +1,28 @@
+//Requires.... The cat variable links to my bots key, as I was strining this dev at the time and did not want everyone to see the key for the bot(Thanks Kat)
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 var cat = require("./safeKek/poop.js");
 var fs = require('fs');
-
+//Requs for the database, linking the database to the db var
 var Datastore = require('nedb')
   , db = new Datastore({ filename: './data.db', autoload: true});
 
+
+//This this prints FUCK. to the console when the bot is loaded successfully.
 bot.on("ready", ()=> {
   console.log('FUCK.')
 });
 
+//This is the event catcher for messages. Any time a message is sent through discord and this picks it up, it runs the contents of the function(message) through it's loops.
+//
 bot.on("message", function(message){
   /*if(message.content === "Hi"){
     bot.reply(message,"Sup, bitch.");
   };*/
+
+  //The ! modifier is just a placeholder. I will probably replace it with "Register", so that it will look like (Register Noita Apex, etc.)
   if(message.content.startsWith("!")) {
+    //Enjoy all of my cluster fuck handler variables for the user registration LOL.
     var usrInfo = (message.content);
     var cmdRemove = usrInfo.substring(1);
     var infoArray = cmdRemove.split(" ");
@@ -25,10 +33,13 @@ bot.on("message", function(message){
     var usrDP = parseInt(infoArray[4]);
     var butts = usrAP+usrDP
 
+    //This is where the "Magic" happens. If and when the user uses the ! modifier, everything following, assuming it's proper, will be stored into the db and printed back using
+    //this clusterfuck of code.
     if(infoArray.length != 5) {
       console.log("incorrect input.")
       bot.reply("You fucked something up please retry.");
     } else {
+      //this is the section for printing back to the discord channel using the reply function
       bot.reply(message," Your stored information is: \n"
       +"Character Name: ***"+usrFirstName+"***\n"
       +"Family Name: ***"+usrLastName+"***\n"
@@ -37,6 +48,7 @@ bot.on("message", function(message){
       +"Last recorded DP: ***"+usrDP+"***\n"
       +"*Combined AP/DP:* "+"***"+butts+"***");
 
+      //this is the bit for storing the db info for the user into the doc array
       var doc = { _id: message.author.id
         , fN: usrFirstName
         , lN: usrLastName
@@ -44,8 +56,10 @@ bot.on("message", function(message){
         , APs: usrAP
         , DPs: usrDP
       };
+      //this is the code for storing the doc array into the database.
       db.insert(doc, function (err, newDoc) {
       });
+      //This was some shit for normal file storage. I'd rather use the database, if I Can get querring to work properly.
       /* Filestore type of shit. Not db.
       fs.appendFile("./infos.txt", message.author.id+","+infoArray+"\n", function(err){
         if(err) {
@@ -58,12 +72,13 @@ bot.on("message", function(message){
     };
   };
 
-    //var cheeky = "'"+message.author.id+"'";
-
+  //This is what I'm fucking mother fuck stuck on god dammit fuck you all.
+  //for some fuck your ass reason I can't get the bot to print the db query(find{Same as SELECT in SQL}) to the discord channel using reply OR message.
+  //I'm 100% positive this is my noobary with JS at work fuck you all :middle_finger:
+  //This is currently preventing me from continuing anything else that I'd like to get done.
     if(message.content.startsWith("poo")) {
-      db.find({ _id: message.author.id }, function (err, doc) {
-
-      });
+       var qt = db.find({_id : message.author.id}, function (err,docs){ console.log(docs); });
+       bot.reply(qt);
     };
 
   console.log(message.author+" "+message.content);
