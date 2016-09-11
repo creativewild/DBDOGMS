952fbecaee5dda51c0be7f3ba4a5547a7fb8ba48
Gray <xxbloodyhell478@gmail.com>
2016-09-11 17:18:05 -0400

    Step 1 of attempting to fix crap.
    This, is really stressful.

--- dbdogms.js
+++ dbdogms.js
@@ -104,7 +104,7 @@ bot.on("message", function(message){
      ],
      mentionable : true
    })
-   bot.sendMessage(message, "Created role: "+classN)
+   message.channel.sendMessage( "Created role: "+classN)
  }
 
   //The ! modifier is just a placeholder. I will probably replace it with "Register", so that it will look like (Register Noita Apex, etc.)
@@ -119,12 +119,12 @@ bot.on("message", function(message){
     //this clusterfuck of code.
     if( infoArray.length != 6 ) {
       console.log("incorrect input.")
-      bot.reply(message,"You've entered your datad with either too many or too little variables. That or it just did not make sense. Please type !Help for more information.").then(bot.deleteMessage(message).catch(function(err){console.log("Error With Delete + Message Process: "+err)}));
+      message.reply("You've entered your datad with either too many or too little variables. That or it just did not make sense. Please type !Help for more information.").then(bot.deleteMessage(message).catch(function(err){console.log("Error With Delete + Message Process: "+err)}));
     } else if( resource1.contains(infoArray[2]) && bot.memberHasRole(message.author.id, message.server.roles.get("name", "Vahlok"))) {
         db.findOne( { _id: message.author.id }, function( err, result ) {
           if( result ) {
             console.log("This user is already in the Database.")
-            bot.reply(message, "You're aleady in the database, no need to use the register command again player ***" + char.familyName + "***").then(bot.deleteMessage(message).catch(function(err){console.log("Error With Delete + Message Process: "+err)}))
+            message.reply( "You're aleady in the database, no need to use the register command again player ***" + char.familyName + "***").then(bot.deleteMessage(message).catch(function(err){console.log("Error With Delete + Message Process: "+err)}))
           } else if ( !err ){
             //this is the section for printing back to the discord channel using the reply function
             var doc = { _id: message.author.id
@@ -138,7 +138,7 @@ bot.on("message", function(message){
             db.insert( doc, function( err, result ) {
               var insChar = new Character( result, 'nedb' );
               if( !err ) {
-                bot.reply(message, "Sending you your registration acknowledgement data.")
+                message.reply( "Sending you your registration acknowledgement data.")
                 bot.sendMessage( message.author.id," ***Your stored information.*** \n"
                           + "```js\nCharacter Name: \"" + char.firstName + "\" \n"
                           + "Family Name: \"" + char.familyName + "\" \n"
@@ -148,17 +148,17 @@ bot.on("message", function(message){
                           + "Last recorded DP: " + char.dp + " \n"
                           + "Combined AP/DP: " + " " + char.adp() + "```" )
                   bot.addMemberToRole(message.author.id, message.server.roles.get("name", insChar.class)).then(bot.deleteMessage(message).catch(function(err){console.log("Error With Delete + Message Process: "+err)}))
-              } else { bot.reply( message, 'Something went wrong...' );
+              } else { message.reply( 'Something went wrong...' );
                 console.log( err );
               }
             });
          } else {
-           bot.reply( message, 'Something went wrong...' );
+           message.reply( 'Something went wrong...' );
            console.log( err );
          }
       })
     } else {
-      bot.reply(message, "\nYou have either entered an unauthorized class, or are not a member of Vahlok rank.\n Please correct your class (Capital first letter, correct spelling) and or mention Fahdon.").then(bot.deleteMessage(message).catch(function(err){console.log("Error With Delete + Message Process: "+err)}))
+      message.reply( "\nYou have either entered an unauthorized class, or are not a member of Vahlok rank.\n Please correct your class (Capital first letter, correct spelling) and or mention Fahdon.").then(bot.deleteMessage(message).catch(function(err){console.log("Error With Delete + Message Process: "+err)}))
     }
   }
 
@@ -166,7 +166,7 @@ bot.on("message", function(message){
     db.findOne({ _id : message.author.id }, function ( err, result ){
       if( result ){
         var char = new Character( result, 'nedb' );
-        bot.reply( message," ***Your stored information.*** \n"
+        message.reply(" ***Your stored information.*** \n"
                   + "```js\nCharacter Name: \"" + char.firstName + "\" \n"
                   + "Family Name: \"" + char.familyName + "\" \n"
                   + "Class: \"" + char.class + "\" \n"
@@ -175,21 +175,21 @@ bot.on("message", function(message){
                   + "Last recorded DP: " + char.dp + " \n"
                   + "Combined AP/DP: " + " " + char.adp() + "```" );
       } else {
-        bot.reply( message, 'Your reccord is not in the database. Have you registered?' );
+        message.reply( 'Your reccord is not in the database. Have you registered?' );
       }
     })
   }
 
   //broken and idk why.
   var rmvRoles = function(rlname) {
-    bot.removeMemberFromRole(message.author.id, message.server.roles.get("name", rlname), function ( err ) {bot.sendMessage(message, "```"+err.stack+"```")})
+    bot.removeMemberFromRole(message.author.id, message.server.roles.get("name", rlname), function ( err ) {message.channel.sendMessage( "```"+err.stack+"```")})
   }
   //Update AP (BASE COMPLETE)
   if(message.content.startsWith('~ap')) {
     var sent = (message.content)
     var person = sent.substring(4, 7)
 
-    db.update({ _id : message.author.id }, { $set: { APs: person } }, { multi: false }, function (err, numReplaced) { if(err){bot.sendMessage(message, err)} else if (numReplaced == "1") { bot.sendMessage(message, numReplaced+" Records Replaced. Please query for updated value.")}})
+    db.update({ _id : message.author.id }, { $set: { APs: person } }, { multi: false }, function (err, numReplaced) { if(err){message.channel.sendMessage( err)} else if (numReplaced == "1") { message.channel.sendMessage( numReplaced+" Records Replaced. Please query for updated value.")}})
 
   }
   //Update DP (BASE COMPLETE)
@@ -197,15 +197,15 @@ bot.on("message", function(message){
     var sent = (message.content)
     var person = sent.substring(4, 7)
 
-    db.update({ _id : message.author.id }, { $set: { DPs: person } }, { multi: false }, function (err, numReplaced) { if(err){bot.sendMessage(message, err)} else if (numReplaced == "1") { bot.sendMessage(message, numReplaced+" Records Replaced. Please query for updated value.")}})
+    db.update({ _id : message.author.id }, { $set: { DPs: person } }, { multi: false }, function (err, numReplaced) { if(err){message.channel.sendMessage( err)} else if (numReplaced == "1") { message.channel.sendMessage( numReplaced+" Records Replaced. Please query for updated value.")}})
 
   }
   //Update Level (BASE COMPLETE)
-  if(message.content.startsWith('~lvl')) {
+  if(message.content.startsWith("~lvl")) {
     var sent = (message.content)
     var person = sent.substring(4, 8)
 
-    db.update({ _id : message.author.id }, { $set: { lvl: person } }, { multi: false }, function (err, numReplaced) { if(err){bot.sendMessage(message, err)} else if (numReplaced == "1") { bot.sendMessage(message, numReplaced+" Records Replaced. Please query for updated value.")}})
+    db.update({ _id : message.author.id }, { $set: { lvl: person } }, { multi: false }, function (err, numReplaced) { if(err){message.channel.sendMessage( err)} else if (numReplaced == "1") { message.channel.sendMessage( numReplaced+" Records Replaced. Please query for updated value.")}})
 
   }
   //Delete From DB Command. (BASE COMPLETE-- Needs a fixin)
@@ -219,14 +219,14 @@ bot.on("message", function(message){
       } else {
         console.log("hmm. Not quite.")
       }
-    } catch (e) {bot.sendMessage(message, "```"+e.stack+"```")}
-    db.remove({ _id : message.author.id }, { multi: true }, function (err, numRemoved) { if(err){bot.sendMessage(message, err)} else if (numRemoved == "0") { bot.sendMessage(message, numRemoved+" Records found. You were not in the Arcane Archives.")} else { bot.sendMessage(message, numRemoved+", Record deleted. You have been removed from my Database Dovahkin.") } })
+    } catch (e) {message.channel.sendMessage( "```"+e.stack+"```")}
+    db.remove({ _id : message.author.id }, { multi: true }, function (err, numRemoved) { if(err){message.channel.sendMessage( err)} else if (numRemoved == "0") { message.channel.sendMessage( numRemoved+" Records found. You were not in the Arcane Archives.")} else { message.channel.sendMessage( numRemoved+", Record deleted. You have been removed from my Database Dovahkin.") } })
     })
   }
 
   if(message.content.startsWith('Create Class Roles Please!') && bot.memberHasRole(message.author.id, message.server.roles.get("name", "Developer"))) {
     console.log("Develoepr has run create role command.")
-    bot.sendMessage(message, "Hello Developer! I am creating your roles now!")
+    message.channel.sendMessage( "Hello Developer! I am creating your roles now!")
     for( var i = 0; i < resource1.length; i++) {
       createClassRoles(resource2[i], resource1[i])
     }
@@ -251,41 +251,41 @@ bot.on("message", function(message){
         //bot.sendMessage("154974615968546816", "```"+monospaceColumnPad("First Name", 13)+monospaceColumnPad("Last Name", 13)+monospaceColumnPad("Class", 10)+monospaceColumnPad("Level", 6)+monospaceColumnPad("AP", 5)+monospaceColumnPad("DP", 5)+monospaceColumnPad("AP/DP", 5)+"\n"+outputShit+"```")
         fs.writeFile("C:/Users/Gray/Documents/Outputs/OutputData.txt", outputShit)
       }})
-      bot.sendMessage(message,"Refreshed User Data File for ***"+moment().format('LLLL')+"***")
+      message.channel.sendMessage("Refreshed User Data File for ***"+moment().format('LLLL')+"***")
   }
   //Temp fix for list all in database.
   if(message.content === "getGI") {
-    bot.sendFile("154974615968546816","C:/Users/Gray/Documents/Outputs/OutputData.txt").catch(function(err){console.log(err)})
-    bot.sendMessage(message, "File Uploaded.")
+    bot.channels.get("154974615968546816").sendFile("C:/Users/Gray/Documents/Outputs/OutputData.txt").catch(function(err){console.log(err)})
+    message.channel.sendMessage( "File Uploaded.")
   }
 
   if(message.content === "!Help") {
-    bot.sendMessage(message, "\n`Register Function: +FirstName LastName Class Level AP DP`\n`Query for your stored information: whoami`\n`Update data: ~AP \(new ap\), ~DP \(new dp\), ~lvl \(new level\)`")
-  }
-
-  //not working. Will have to fix another time. Supposed to be fore deleting messages and shit lol.
-  if(message.content === "FDSUCHANNEL" & message.author.id === "92738910362292224") {
-    var meowth = bot.getChannelLogs(message.channel, 5).catch(function(err) {
-      console.log("Error getting lgos: ", err)
-    }).then(function(logs){
-      //bot.sendMessage(message, logs)
-      fs.writeFile("./cats", logs).catch(function(err){console.log("error creating/writing file: " + err)})
-    })
-    //bot.deleteMessages(meowth).catch(function(err){bot.sendMessage(message, "error deleting last 5 messages: "+ err)})
+    message.channel.sendMessage( "\n`Register Function: +FirstName LastName Class Level AP DP`\n`Query for your stored information: whoami`\n`Update data: ~AP \(new ap\), ~DP \(new dp\), ~lvl \(new level\)`")
   }
 
   if(message.content === "setDev" & message.author.id === "92738910362292224") {
-    bot.setStatus("away", "Frequent Restarts.").catch(function(err){console.log("error with status setting 1: "+ err)})
+    bot.user.setStatus("away", "Frequent Restarts.").catch(function(err){console.log("error with status setting 1: "+ err)})
+    console.log("Development Mode Set.")
   } else if (message.content === "setFree"){
-    bot.setStatus("active", "Persist mode. Notify Dr. Gray of downtime/issues.").catch(function(err){console.log("error with status setting 2: "+ err)})
+    bot.user.setStatus("active", "Persist mode. Notify Dr. Gray of downtime/issues.").catch(function(err){console.log("error with status setting 2: "+ err)})
+    console.log("Persistent Mode Set.")
   }
 
+  if(message.content === "C") {
+    if(message.member.roles.get("name", "Hoodrat") === true){
+      console.log("pass")
+    } else {
+      console.log("fail")
+    }
+  }
+
+/*
   if(message.channel.isPrivate === true & message.author.id != "209020857446694913"){
     console.log("Private Message")
   }else if (message.author.id != "209020857446694913") {
     console.log("["+c.cyanBright.bold(message.author.username)+"] Sent:\n\n"+c.greenBright.italic(message.content)+"\n\nOn server: "+c.redBright(message.server.name)+"\nOn Channel: "+c.redBright(message.channel.name)+"\nAt time: "+c.redBright(moment().format('LLLL')));
     console.log("-------------------------------------------------------------------------")
   }
-
+*/
 });
-bot.loginWithToken( Auth.token );
+bot.login( Auth.token );
